7b5ad4a4eeed6da46bd765c6b0401adfc2199202
Stephen Brooks <osldgoth@gmail.com>
2023-05-03 12:24:21 -0400

    added some notes on variable naming conventions

--- src/hamburger.js
+++ src/hamburger.js
@@ -1,4 +1,22 @@
 
+/*
+notes:
+Naming via https://dev.to/samuel-braun/naming-every-developers-nightmare-3ge8
+[scope(private, public, protected, internal, $, @, __, use, etc)]
+[typePrefex(is, has, contains, are , integrations, get, set, fetch, update, store, save...)]
+[baseName(user, distance, payment, errorMessage, status, price, elapsedTime...)]
+[qualifier(firstName, lastName, phoneNumber, valid, in, byId...)]
+[typeSuffix(count, index, sum, average, list, map, array...)]
+
+[scope][typePre][basename][qualifier]
+__isUserLoggedin
+[scope][typePre][baseName][qualifier]
+(public)fetchProductByID
+[scope][prefix][basename][qualifier]
+""updateEmailPref
+
+*/
+
 const menu = document.querySelector(".menu"); //hamburger
 const nav = document.querySelector(".navigation"); //why again?
 
